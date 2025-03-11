690001baa880493defe94a552dce9e05185a6788
Purvank Javade <purvank.javade@radixweb.com>
2025-03-11 17:55:25 +0530

    fix(ui): prev-next ui changes

--- pages/docs/[...slug].vue
+++ pages/docs/[...slug].vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
-import TocLinks from '~/components/TocLinks.vue'
-import Hint from '~/components/Hint.vue'
-import Figure from '~/components/Figure.vue'
+  import TocLinks from '~/components/TocLinks.vue'
+  import Hint from '~/components/Hint.vue'
+  import Figure from '~/components/Figure.vue'
 
-const components = {
-  'hint': Hint,
-  'fiqure-img': Figure,
-}
+  const components = {
+    hint: Hint,
+    'fiqure-img': Figure,
+  }
 
-const route = useRoute()
+  const route = useRoute()
 
-const params = (route.params['slug'] as string[]) || []
+  const params = (route.params['slug'] as string[]) || []
 
-// if (params.length) params.pop();
+  // if (params.length) params.pop();
 
-const title = params[0]
-  .split('-') // Split by hyphen
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
-  .join(' ') // Join words with space
+  const title = params[0]
+    .split('-') // Split by hyphen
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+    .join(' ') // Join words with space
 
-const { data: page } = await useAsyncData(`docs-${route.path}`, () =>
-  queryCollection('content').path(route.path).first(),
-)
+  const { data: page } = await useAsyncData(`docs-${route.path}`, () =>
+    queryCollection('content').path(route.path).first()
+  )
 
-definePageMeta({
-  layout: 'docslayout',
-})
+  definePageMeta({
+    layout: 'docslayout',
+  })
 
-const { data: surround } = await useAsyncData(`${route.path}-surround`, () =>
-  queryCollectionItemSurroundings('content', route.path),
-)
+  const { data: surround } = await useAsyncData(`${route.path}-surround`, () =>
+    queryCollectionItemSurroundings('content', route.path)
+  )
 
-const [prev, next] = surround.value || []
+  const [prev, next] = surround.value || []
 
-useSeoMeta({
-  title: page.value?.title,
-  description: page.value?.description,
-})
+  useSeoMeta({
+    title: page.value?.title,
+    description: page.value?.description,
+  })
 </script>
 
 <template>
@@ -48,10 +48,7 @@ useSeoMeta({
         </div>
         <div class="flex items-center">
           <span class="text-3xl text-gray-500">
-            <font-awesome-icon
-              :icon="page?.meta.icon"
-              class="pe-3 pt-6"
-            />
+            <font-awesome-icon :icon="page?.meta.icon" class="pe-3 pt-6" />
           </span>
           <h1 class="text-3xl text-gray-800 pt-5 font-zt_bold">
             {{ page?.title }}
@@ -62,11 +59,7 @@ useSeoMeta({
           class="text-base text-[#3c3c43] tracking-wide leading-7 pb-2 m-0"
           :class="page?.description ? 'mt-3' : 'm-0'"
         >
-          <MDC
-            v-if="page?.description"
-            :value="page?.description"
-            unwrap="p"
-          />
+          <MDC v-if="page?.description" :value="page?.description" unwrap="p" />
         </p>
       </div>
       <ContentRenderer
@@ -78,30 +71,18 @@ useSeoMeta({
       />
       <!-- <ContentRendererMarkdown id="contentrenderer" :value="page" :components="components" />
         </ContentRenderer> -->
-      <hr v-if="surround?.length">
-      <div
-        v-if="surround"
-        class="surround-div flex justify-end my-5 gap-x-5"
-      >
+      <hr v-if="surround?.length" />
+      <div v-if="surround" class="surround-div flex justify-end my-5 gap-x-5">
         <div class="w-1/2">
           <div
             v-if="prev"
-            class="prev border rounded-md hover:bg-gray-50/30 group p-1 transition-all duration-300 hover:border-zt_purple/10"
+            class="prev border rounded-md hover:bg-gray-50/30 group p-1 transition-all duration-300 hover:border-zt_purple/70"
           >
             <NuxtLink :to="prev.path">
-              <div
-                v-if="prev"
-                class="surround-prev flex justify-between items-center gap-x-3"
-              >
-                <font-awesome-icon
-                  icon="angles-left"
-                  class="text-gray-600 transition-all duration-300 group-hover:text-zt_purple text-sm rounded-full group-hover:bg-slate-100 px-4 py-3.5"
-                />
-                <div class="text-right px-4 py-2.5 space-y-1">
-                  <span class="text-xs text-gray-400">Previous</span>
-                  <h5
-                    class="text-gray-800 group-hover:text-zt_purple w-full text-right text-base font-zt_medium"
-                  >
+              <div v-if="prev" class="surround-prev">
+                <div class="px-3 py-1">
+                  <span class="text-xs text-gray-500 font-zt_regular tracking-wide">Previous</span>
+                  <h5 class="text-zt_purple text-base font-zt_medium">
                     {{ prev.title }}
                   </h5>
                 </div>
@@ -112,25 +93,16 @@ useSeoMeta({
         <div class="w-1/2">
           <div
             v-if="next"
-            class="next text-right border rounded-md hover:bg-slate-50/30 p-1 group transition-all duration-300 hover:border-zt_purple/10"
+            class="next text-right border rounded-md hover:bg-slate-50/30 p-1 group transition-all duration-300 hover:border-zt_purple/70"
           >
             <NuxtLink :to="next.path">
-              <div
-                v-if="next"
-                class="surround-next flex justify-between items-center gap-x-3 transition-all duration-300"
-              >
-                <div class="text-left px-4 py-2.5 space-y-1">
-                  <span class="text-xs text-gray-400">Next</span>
-                  <h5
-                    class="text-gray-800 font-zt_medium text-base group-hover:text-zt_purple w-full"
-                  >
+              <div v-if="next" class="surround-next">
+                <div class="px-3 py-1">
+                  <span class="text-xs text-gray-500 font-zt_regular tracking-wide">Next</span>
+                  <h5 class="text-zt_purple font-zt_medium text-base">
                     {{ next.title }}
                   </h5>
                 </div>
-                <font-awesome-icon
-                  icon="angles-right"
-                  class="text-gray-600 group-hover:text-zt_purple text-sm transition-all duration-300 rounded-full group-hover:bg-slate-100 px-4 py-3.5"
-                />
               </div>
             </NuxtLink>
           </div>
@@ -138,10 +110,7 @@ useSeoMeta({
       </div>
     </div>
     <div class="col-span-3 flex justify-start">
-      <div
-        v-if="page?.body?.toc?.links"
-        class="min-w-52"
-      >
+      <div v-if="page?.body?.toc?.links" class="min-w-52">
         <TocLinks :toclinks="page.body.toc.links" />
       </div>
     </div>
