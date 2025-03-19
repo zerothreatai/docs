15222626a0e0bb8bc1c03452f6b0a58e1acc29ba
JeelGajera <jeelgajera200@gmail.com>
2025-03-19 14:14:29 +0530

    feat: setup algolia search model

---
+++ components/AlgoliaSearchModel.vue
@@ -0,0 +1,48 @@
+<!-- eslint-disable @typescript-eslint/no-explicit-any -->
+<script setup lang="ts">
+const searchComponent = ref(null)
+
+// Function to focus the hidden input and open the modal
+const openSearch = () => {
+  if (searchComponent.value) {
+    // Find the input element within AlgoliaDocSearch
+    const input = (searchComponent.value as any).$el.querySelector('input')
+    if (input) {
+      input.focus() // Focusing the input should trigger the modal
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="w-full flex justify-center">
+    <AlgoliaDocSearch
+      ref="searchComponent"
+      placeholder="Search Documentation"
+    />
+    <div
+      class="px-3 py-2 w-full max-w-xl cursor-pointer flex items-center justify-between border border-slate-200 rounded-md bg-white shadow-sm"
+      @click="openSearch"
+    >
+      <div class="flex items-center gap-x-3">
+        <span><font-awesome-icon
+          icon="magnifying-glass"
+          class="text-gray-400 translate-y-0.5"
+        /></span>
+        <span class="text-gray-400">Search Documentation</span>
+      </div>
+      <div class="flex items-center gap-x-1">
+        <span
+          class="text-xs font-medium text-gray-500 px-1 text-center rounded bg-slate-50 border border-gray-300"
+        >Ctrl</span>
+        <span
+          class="text-xs font-medium text-gray-500 px-1 text-center rounded bg-slate-50 border border-gray-300"
+        >k</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,32 +1,7 @@
 <script setup lang="ts">
 // import ContentSearchButton from './ContentSearchButton.vue'
 import githubIcon from './icon/github.icon.vue'
-import SearchModal from './SearchModal.vue'
-
-const showSearchModal = ref(false)
-
-const toggleSearch = () => {
-  showSearchModal.value = !showSearchModal.value
-  if (showSearchModal.value) {
-    document.body.classList.add('overflow-hidden')
-  }
-  else {
-    document.body.classList.remove('overflow-hidden')
-  }
-}
-const handleKeydown = (event: KeyboardEvent) => {
-  if (event.ctrlKey && event.key === 'k') {
-    event.preventDefault()
-    toggleSearch()
-  }
-}
-onMounted(() => {
-  window.addEventListener('keydown', handleKeydown)
-})
-
-onBeforeUnmount(() => {
-  window.removeEventListener('keydown', handleKeydown)
-})
+import AlgoliaSearchModel from './AlgoliaSearchModel.vue'
 </script>
 
 <template>
@@ -47,42 +22,16 @@ onBeforeUnmount(() => {
           </NuxtLink>
         </div>
         <!-- search -->
-        <div class="w-full flex justify-center">
-          <div
-            class="px-3.5 py-2 w-full max-w-xl cursor-pointer flex items-center justify-between border border-slate-200 rounded-md bg-white shadow-sm"
-            @click="toggleSearch"
-          >
-            <div class="flex items-center gap-x-3">
-              <span><font-awesome-icon
-                icon="magnifying-glass"
-                class="text-gray-400 translate-y-0.5"
-              /></span>
-              <span class="text-gray-400">Search Documentation</span>
-            </div>
-            <div class="flex items-center gap-x-1">
-              <span
-                class="text-xs font-medium text-gray-500 px-1 text-center rounded bg-slate-50 border border-gray-300"
-              >
-                Ctrl
-              </span>
-              <span
-                class="text-xs font-medium text-gray-500 px-1 text-center rounded bg-slate-50 border border-gray-300"
-              >
-                k
-              </span>
-            </div>
-          </div>
-        </div>
+        <AlgoliaSearchModel />
         <div>
-          <a href="https://github.com/zerothreatai" target="_blank">
+          <a
+            href="https://github.com/zerothreatai"
+            target="_blank"
+          >
             <github-icon class="w-6 fill-gray-700 hover:fill-zt_purple cursor-pointer" />
           </a>
         </div>
       </div>
-      <SearchModal
-        v-if="showSearchModal"
-        @close="toggleSearch"
-      />
     </div>
   </header>
 </template>
