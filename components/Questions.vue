de6b7c1722d2abb548d418681c0ed160e40200bc
sayanichopra <sayanichopra18@gmail.com>
2025-05-28 14:01:09 +0530

    fix : faqs search functionality

--- components/Questions.vue
+++ components/Questions.vue
@@ -16,14 +16,15 @@ const selectButton = async (tab) => {
       const res = await fetch(`/${tab.file}`)
       const jsonData = (await res.json()).map(d => ({ ...d, isOpen: false }))
       // console.log(jsonData)
-      filteredfaqs.value = jsonData ? searchQuery.value.trim().length ? jsonData.filter(d => d.q.lowerCase().includes(searchQuery.value.trim().toLowerCase())) : jsonData : []
+      filteredfaqs.value = jsonData ? searchQuery.value.trim().length ? jsonData.filter(d => d.q.toLowerCase().includes(searchQuery.value.trim().toLowerCase())) : jsonData : []
     }
     else {
       filteredfaqs.value = []
     }
   }
-  catch {
+  catch (err) {
     filteredfaqs.value = []
+    jsonData = []
   }
 }
 
@@ -56,7 +57,11 @@ const toggleAccordion = (item) => {
 }
 
 const filteredfaqsSearch = () => {
-  filteredfaqs.value = filteredfaqs.value.filter(d => d.q.lowerCase().includes(searchQuery.value.trim().toLowerCase()))
+  if (!searchQuery.value.trim().length) {
+    filteredfaqs.value = jsonData.map(d => ({ ...d, isOpen: false }))
+    return
+  }
+  filteredfaqs.value = jsonData.value.filter(d => d.q.toLowerCase().includes(searchQuery.value.trim().toLowerCase()))
 }
 </script>
 
@@ -79,7 +84,7 @@ const filteredfaqsSearch = () => {
             type="text"
             placeholder="How can we help you today ?"
             class="text-sm w-full placeholder:text-gray-400 placeholder:font-zt_regular text-gray-700 font-zt_medium outline-none"
-            @change="() => filteredfaqsSearch()"
+            @input="filteredfaqsSearch"
           >
         </div>
         <!-- tabs -->
