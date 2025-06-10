4d3ef48ebd081ca39299ca0884252d715692fcb1
vijay.vaghela <vijay.vaghela@radixweb.com>
2025-06-10 11:35:27 +0530

    fix : pre-code-ui

--- components/UCode.vue
+++ components/UCode.vue
@@ -30,18 +30,32 @@ function copytoclipboard() {
 </script>
 
 <template>
-  <div class="relative border rounded-md bg-gray-50 ">
-    <div class="flex items-center justify-between px-2 pt-2">
+  <div class="relative border rounded-md bg-gray-50 mb-5">
+    <div class="flex items-center w-full justify-end px-3 pt-3">
       <span @click="copytoclipboard"><copy-icon
         class="w-4 fill-gray-500 hover:fill-gray-700 cursor-pointer pb-2"
       /></span>
     </div>
-    <div class="px-5 py-2  rounded-b-md text-xl">
+    <div id="code-block" class="px-5 rounded-b-md text-xl ">
       <component
         :is="tabs?.component"
         :key="tabs.label"
-        class="gap-32"
+        class="p-0 m-0 h-fit text-wrap pb-5"
       />
     </div>
   </div>
 </template>
+
+<style>
+#code-block pre code {
+font-size: 0.8rem !important;
+border: none !important;
+line-height: 1rem !important;
+}
+#code-block pre code {
+padding: 0px !important;
+}
+#code-block pre code span {
+line-height: 1.2rem !important;
+}
+</style>
