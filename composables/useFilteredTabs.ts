3197508263d7ab79622eeddda1af62d1c54a7b07
vijay.vaghela <vijay.vaghela@radixweb.com>
2026-06-30 15:47:18 +0530

    feat(ui): add API Security and Promos tabs to FAQ

--- composables/useFilteredTabs.ts
+++ composables/useFilteredTabs.ts
@@ -7,9 +7,7 @@ export function useFilteredTabs() {
   const { public: { appEnv } } = useRuntimeConfig()
 
   const irrelevantCategories = [
-    FaqCategory['API Security'],
     FaqCategory.Integrations,
-    FaqCategory.Promos,
   ]
 
   const filteredTabs = computed(() => {
